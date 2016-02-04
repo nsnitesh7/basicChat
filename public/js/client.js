3565417e6f0cda458147442208c3b93a91010103
Nitesh Singhal <nsinghal@zenefits.com>
2016-02-03 20:23:10 -0800

    basic chat complete

---
+++ public/js/client.js
@@ -0,0 +1,74 @@
+;
+(function() {
+    //get required nodes
+    function getNode(s) {
+        return document.querySelector(s);
+    };
+    var textarea = getNode('.chat-textarea');
+    var chatName = getNode('.chat-name');
+    var statusDisplay = getNode('.chat-status');
+    var messages = getNode('.chat-messages');
+
+    var defaultStatus = statusDisplay.textContent;
+
+    function setStatus(s) {
+        statusDisplay.textContent = s.message;
+        console.log(s.clear);
+        if (s.clear) {
+            textarea.value = '';
+        }
+        setTimeout(function() {
+            statusDisplay.textContent = defaultStatus;
+        }, 5000);
+
+    }
+
+
+    try {
+        var r=window.location.pathname;
+        r= r.substr(1);
+        var socket = io.connect('/', {query: "room="+r});
+    } catch (e) {
+        console.error(e);
+        //set display status
+    }
+    if (socket) {
+        console.log('socket ok');
+
+        socket.on('status', function(s) {
+            setStatus(s);
+        });
+
+        socket.on('output', function(data) {
+            console.log(data);
+            if (data.length) {
+                //loop through the results
+                for (var i = 0; i < data.length; i++) {
+                    var message = document.createElement('div');
+                    message.setAttribute('class', 'chat-message');
+                    message.textContent = data[i].name + ': ' + data[i].message;
+
+                    //append to messages list
+                    messages.appendChild(message);
+                    messages.insertBefore(message, messages.firstChild);
+                }
+            }
+        });
+
+
+        textarea.addEventListener('keydown', function(event) {
+
+            if (event.which === 13 && event.shiftKey === false) {
+                console.log('send');
+                var r=window.location.pathname;
+                r= r.substr(1);
+                socket.emit('input', {
+                    room: r,
+                    name: chatName.value,
+                    message: textarea.value
+                });
+            }
+
+        });
+    }
+})();
